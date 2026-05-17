ce913be7c029cb9585c575e99865a56e94221da5
LeeNgocQuyet <159886685+LeeNgocQuyet@users.noreply.github.com>
2026-05-17 20:45:06 +0700

    Feat `MainActivity` to initialize `AppContainer` and set up application navigation

--- app/src/main/java/com/project/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/project/cryptoapp/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.project.cryptoapp.ui.theme.CryptoAppTheme
+import com.project.cryptoapp.presentation.navigation.AppNavigation
+import com.project.cryptoapp.presentation.theme.ECC512CryptoTheme
+import com.project.cryptoapp.util.AppContainer
 
 class MainActivity : ComponentActivity() {
+    private lateinit var appContainer: AppContainer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        appContainer = AppContainer(applicationContext)
         enableEdgeToEdge()
         setContent {
-            CryptoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            ECC512CryptoTheme {
+                AppNavigation(appContainer = appContainer)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoAppTheme {
-        Greeting("Android")
     }
 }
